c6809803c084446a533d5c7b14f07fa6aaf6d301
id23 <aleks-s-82@bk.ru>
2015-09-01 14:22:41 +0300

    rename functionsin verb format

--- web_server.py
+++ web_server.py
@@ -16,14 +16,14 @@ def lenght (fileaddr):
     filelenght = len(file.read())
     return filelenght
 
-def site_execution (fileaddr,content_type,date):
+def execute_site (fileaddr,content_type,date):
     if os.path.isfile(fileaddr) == True:
         header = 'HTTP/1.1 200 OK\r\n' \
                  'Server: Myserver\r\n ' \
                  'Date: {0}\r\n ' \
                  'Content-Type: '+ content_type +'\r\n' \
                  'Content-Length:{1}\r\n\r\n'.format(date, lenght(fileaddr))
-        file_open(fileaddr, header)
+        open_file(fileaddr, header)
     else:
         header = 'HTTP/1.1 404 Not Found\r\n' \
 
@@ -31,7 +31,7 @@ def site_execution (fileaddr,content_type,date):
 
 
 
-def file_open(fileaddr, header):
+def open_file(fileaddr, header):
     page = open(fileaddr)
     page_content = page.read()
     print "Connection from: " + `caddr`
@@ -39,7 +39,7 @@ def file_open(fileaddr, header):
     csock.sendall(page_content)
 
 
-def content_type_define(file_type_str):
+def define_content_type(file_type_str):
     content_type = ''
     if file_type_str == '/' or file_type_str == 'html' or file_type_str == '':
         content_type = 'text/html'
@@ -50,13 +50,13 @@ def content_type_define(file_type_str):
     return content_type
 
 
-def file_type_define(file_name_str):
+def define_file_type(file_name_str):
     file_type_index = file_name_str.find(".")
     file_type = file_name_str[file_type_index + 1:]
     return file_type
 
 
-def file_address_define(file_name_str):
+def define_file_address(file_name_str):
     file_address = document_root + file_name_str
     if file_name_str == '/':
         file_address = document_root + default_page
@@ -67,13 +67,15 @@ while True:
 
     csock, caddr = sock.accept()
     req = csock.recv(1024)
-    if req == '':
-        csock.close()
-    else:
+    if len(req) > 0:
         lines = req.split()
         file_name = lines[1]
-        file_address = file_address_define(file_name)
-        file_type = file_type_define(file_name)
-        content_type = content_type_define(file_type)
-        site_execution (file_address,content_type,date)
-        csock.close()
+        file_address = define_file_address(file_name)
+        file_type = define_file_type(file_name)
+        content_type = define_content_type(file_type)
+        execute_site (file_address,content_type,date)
+
+    csock.close()
+
+
+
