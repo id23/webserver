aad11c110ad926aec864d01c49f5fd41f78c0685
id23 <aleks-s-82@bk.ru>
2015-09-01 11:56:10 +0300

    optimised version

--- web_server.py
+++ web_server.py
@@ -15,11 +15,36 @@ def lenght (fileaddr):
     filelenght = len(file.read())
     return filelenght
 
+def site_execution (fileaddr,content_type,date):
+    if os.path.isfile(fileaddr) == True:
+        header = 'HTTP/1.1 200 OK\r\n' \
+                 'Server: Myserver\r\n ' \
+                 'Date: {0}\r\n ' \
+                 'Content-Type: '+ content_type +'\r\n' \
+                 'Content-Length:{1}\r\n\r\n'.format(date, lenght(fileaddr))
+        page_open(fileaddr, header)
+    else:
+        header = 'HTTP/1.1 404 Not Found\r\n' \
+                 'Server: Myserver\r\n' \
+                 'Date: {0}\r\n' \
+                 'Content-Type: Text/html'.format(date)
+        csock.sendall(header)
+
+
+
+def page_open(fileaddr, header):
+    page = open(fileaddr)
+    page_content = page.read()
+    print "Connection from: " + `caddr`
+    csock.sendall(header)
+    csock.sendall(page_content)
+
+
 while True:
+
+    lines = "john"
     csock, caddr = sock.accept()
-
     req = csock.recv(1024)
-
     lines = req.split()
     content_type = ''
     file_name_list = lines[1]
@@ -27,29 +52,14 @@ while True:
     if file_name_list == '/':
         file_address = document_root + '/page.html'
     file_type_index = file_name_list.find(".")
-    file_type_list = file_name_list[file_type_index + 1:]
+    file_type_str = file_name_list[file_type_index + 1:]
 
-    header_get = "HTTP/1.1 200 OK\r\n"
-
-    if file_type_list == '/' or file_type_list == 'html' or file_type_list == '':
+    if file_type_str == '/' or file_type_str == 'html' or file_type_str == '':
         content_type = 'text/html'
-    elif file_type_list == 'jpg' or file_type_list == 'jpeg':
+    elif file_type_str == 'jpg' or file_type_str == 'jpeg':
         content_type = 'image/jpeg'
-
-    if os.path.isfile(file_address) == False:
-        header_get = 'HTTP/1.1 404 Not Found \r\n'
-        file_address = document_root + '/404page.html'
-        content_type = 'text/html'
     if content_type == '':
         content_type = 'text/html'
-    header = header_get + \
-             'Server: Myserver\r\n' \
-             'Date: {0}\r\n' \
-             'Content-Type: '+ content_type +'\r\n' \
-             'Content-Length:{1}\r\n\r\n'.format(date, lenght(file_address))
-    page = open(file_address)
-    page_content = page.read()
-    print "Connection from: " + `caddr`
-    csock.sendall(header)
-    csock.sendall(page_content)
+
+    site_execution (file_address,content_type,date)
     csock.close()
