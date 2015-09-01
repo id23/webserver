01e361c8aed30d7feb73b6c514a50dad0c4ac03c
id23 <aleks-s-82@bk.ru>
2015-09-01 14:08:52 +0300

    MORE optimised version

--- web_server.py
+++ web_server.py
@@ -2,6 +2,7 @@ import socket
 import datetime
 import os.path
 document_root = '/etc/page'
+default_page = '/page.html'
 host = 'localhost'
 port = 1026
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -22,17 +23,15 @@ def site_execution (fileaddr,content_type,date):
                  'Date: {0}\r\n ' \
                  'Content-Type: '+ content_type +'\r\n' \
                  'Content-Length:{1}\r\n\r\n'.format(date, lenght(fileaddr))
-        page_open(fileaddr, header)
+        file_open(fileaddr, header)
     else:
         header = 'HTTP/1.1 404 Not Found\r\n' \
-                 'Server: Myserver\r\n' \
-                 'Date: {0}\r\n' \
-                 'Content-Type: Text/html'.format(date)
+
         csock.sendall(header)
 
 
 
-def page_open(fileaddr, header):
+def file_open(fileaddr, header):
     page = open(fileaddr)
     page_content = page.read()
     print "Connection from: " + `caddr`
@@ -41,7 +40,7 @@ def page_open(fileaddr, header):
 
 
 def content_type_define(file_type_str):
-    global content_type
+    content_type = ''
     if file_type_str == '/' or file_type_str == 'html' or file_type_str == '':
         content_type = 'text/html'
     elif file_type_str == 'jpg' or file_type_str == 'jpeg':
@@ -52,31 +51,29 @@ def content_type_define(file_type_str):
 
 
 def file_type_define(file_name_str):
-    global file_type_str
     file_type_index = file_name_str.find(".")
-    file_type_str = file_name_str[file_type_index + 1:]
-    return file_type_str
+    file_type = file_name_str[file_type_index + 1:]
+    return file_type
 
 
 def file_address_define(file_name_str):
-    global file_address
-    #file_name_str = lines[1]
     file_address = document_root + file_name_str
     if file_name_str == '/':
-        file_address = document_root + '/page.html'
+        file_address = document_root + default_page
     return file_address
 
 
 while True:
 
-    lines = "john"
     csock, caddr = sock.accept()
     req = csock.recv(1024)
-    lines = req.split()
-    file_name_str = lines[1]
-    file_address = file_address_define(file_name_str)
-    file_type_str = file_type_define(file_name_str)
-    content_type = content_type_define(file_type_str)
-
-    site_execution (file_address,content_type,date)
-    csock.close()
+    if req == '':
+        csock.close()
+    else:
+        lines = req.split()
+        file_name = lines[1]
+        file_address = file_address_define(file_name)
+        file_type = file_type_define(file_name)
+        content_type = content_type_define(file_type)
+        site_execution (file_address,content_type,date)
+        csock.close()
